7cdbb8d5df5f6c8e920dae12fd5376d6b47fda68
Alena Kuimova <alvikui@yandex.ru>
2024-02-02 13:34:10 +0300

    Initial commit

---
+++ cypress/e2e/favoriteBook.cy.js
@@ -0,0 +1,44 @@
+const bookFirst = {
+  title: "Алмазный Огранщик",
+  description:
+    "Система управления бизнесом и жизнью",
+  author: "Майкл Роуч",
+};
+
+const bookSecond = {
+  title: "Пока-я-не-Я",
+  description:
+    "Практическое руководство по трансформации судьбы",
+  author: "Дмитрий Троцкий",
+};
+
+describe("Избранное", () => {
+  beforeEach(() => {
+    cy.visit('http://localhost:3000')
+    cy.login("bropet@mail.ru", "123")
+  });
+
+
+  it("Добавить новую книгу в избранное", () => {
+    cy.addFavoriteBook(bookSecond);
+    cy.visit("/favorites");
+    cy.get(".card-title").should("contain.text", bookSecond.title);
+  });
+
+  it("Добавить книгу в избранное из списка книг", () => {
+    cy.addBookNoFavorite(bookFirst);
+    cy.contains(bookFirst.title)
+      .should("be.visible")
+      .within(() => cy.get(".card-footer > .btn").click({ force: true }));
+    cy.visit("/favorites");
+    cy.contains(bookFirst.title).should("be.visible");
+  });
+
+  it("Удалить книгу из избранного", () => {
+    cy.visit("/favorites");
+    cy.contains(bookSecond.title)
+      .should("be.visible")
+      .within(() => cy.get(".card-footer > .btn").click({ force: true }));
+    cy.contains(bookSecond.title).should("not.exist");
+  });
+});
---
+++ cypress/e2e/login.cy.js
@@ -0,0 +1,32 @@
+describe('template spec', () => {
+  beforeEach( () => {
+    cy.visit('http://localhost:3000')
+  })
+
+  it('Нормальный вход', () => {
+    cy.login("bropet@mail.ru", "123")
+    cy.contains('Добро пожаловать bropet@mail.ru').should('be.visible')
+  })
+
+  it('Пустой email', () => {
+    cy.login(null, "123")
+    cy.get("#mail").then( (elemenst) => {
+      expect(elemenst[0].checkValidity()).to.be.false
+      expect(elemenst[0].validationMessage).to.be.eql("Please fill out this field.")
+    })
+  })
+
+  it('Пустой пароль', () => {
+    cy.login("bropet@mail.ru",null)
+    cy.get("#pass").then( (elemenst) => {
+      expect(elemenst[0].checkValidity()).to.be.false
+      expect(elemenst[0].validationMessage).to.be.eql("Please fill out this field.")
+    })
+  })
+
+
+
+
+
+
+})
